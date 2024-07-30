436b7d219a3a940992539fefdb9a390b38bef6ef
RafaTeron <rafael_sabreu@hotmail.com>
2024-07-30 13:31:24 -0300

    Reparando(Parte 1)

--- src/main/java/com/rafaelAbreu/LoginJWT/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/rafaelAbreu/LoginJWT/infra/security/CustomUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import com.rafaelAbreu.LoginJWT.entities.User;
 import com.rafaelAbreu.LoginJWT.repositories.UserRepository;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository repository;
